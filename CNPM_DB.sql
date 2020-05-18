efee024fc65e89a7b6cfa05cd2531b6f21c5c7cd
Lê Xuân Thuận <48992868+Edward-Lee-1203@users.noreply.github.com>
2020-05-18 21:28:25 +0700

    adding DB querry

---
+++ CNPM_DB.sql
@@ -0,0 +1,53 @@
+use DA_CNPM
+create table UserTb
+(
+	userId int primary key,
+	userName nvarchar(255),
+	passwordUser nvarchar(50),
+	fullName nvarchar(255),
+	dateOfBirth date,
+	gender bit,
+	addressUser nvarchar(255),
+	phoneNumber nvarchar(11),
+	creditPoint nvarchar(20)
+)
+GO 
+create table ServiceTb
+(
+	serviceId int primary key,
+	serviceName nvarchar(255),
+	price int,
+	regulation nvarchar(500)
+)
+GO
+create table PartnerTb
+(
+	partnerId int primary key,
+	activePlace nvarchar(255),
+	review nvarchar(500),
+	addressPartner nvarchar(255),
+	phoneNumber nvarchar(11),
+	activeTime nvarchar(255),
+)
+GO
+create table BillTb
+(
+	billId int primary key,
+	timeBill date,
+	totalPrice int,
+	serviceId int,
+	partnerId int,
+	userId int,
+	foreign key (serviceId) references ServiceTb(serviceId),
+	foreign key (partnerId) references PartnerTb(partnerId),
+	foreign key (userId) references UserTb(userId),
+)
+create table ServiceOfPartnerTb
+(
+	serviceOfPartnerId int primary key,	totalPrice int,
+	serviceId int,
+	partnerId int,
+	foreign key (serviceId) references ServiceTb(serviceId),
+	foreign key (partnerId) references PartnerTb(partnerId),
+)
+
