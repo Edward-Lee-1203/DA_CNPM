ebb60bcf84aac7c2999236168afb0b07511c0533
Lê Xuân Thuận <48992868+Edward-Lee-1203@users.noreply.github.com>
2020-05-19 15:21:14 +0700

    modify: del serviceId in billTb and del activePlace in partnerTb

--- CNPM_DB.sql
+++ CNPM_DB.sql
@@ -23,7 +23,6 @@ GO
 create table PartnerTb
 (
 	partnerId int primary key,
-	activePlace nvarchar(255),
 	review nvarchar(500),
 	addressPartner nvarchar(255),
 	phoneNumber nvarchar(11),
@@ -35,10 +34,8 @@ create table BillTb
 	billId int primary key,
 	timeBill date,
 	totalPrice int,
-	serviceId int,
 	partnerId int,
 	userId int,
-	foreign key (serviceId) references ServiceTb(serviceId),
 	foreign key (partnerId) references PartnerTb(partnerId),
 	foreign key (userId) references UserTb(userId),
 )
@@ -58,4 +55,30 @@ insert into UserTb values(2, 'thuanmapqn1', '120300', 'Lê Xuân Thu', '2000-03-
 insert into UserTb values(3, 'thuanmapqn2', '120300', 'Nguyên Văn A', '2000-03-12', 1, 'K64 Trân Đinh Tri', '0889709099', '21233434255')
 insert into UserTb values(4, 'thuanmapqn3', '120300', 'Lê Văn B', '2000-03-12', 1, 'K64 Trân Đinh Tri', '0889709099', '21233434255')
 insert into UserTb values(5, 'thuanmapqn4', '120300', 'Lê Xuân C', '2000-03-12', 1, 'K64 Trân Đinh Tri', '0889709099', '21233434255')
+
+insert into PartnerTb values(1, 'best', 'K64 Trân Đinh Tri', '0889304834','12am - 18pm')
+insert into PartnerTb values(2, 'best', 'K64 Trân Đinh Tri', '0889304834','12am - 18pm')
+insert into PartnerTb values(3, 'best', 'K64 Trân Đinh Tri', '0889304834','12am - 18pm')
+insert into PartnerTb values(4, 'best', 'K64 Trân Đinh Tri', '0889304834','12am - 18pm')
+insert into PartnerTb values(5, 'best', 'K64 Trân Đinh Tri', '0889304834','12am - 18pm')
+
+insert into ServiceTb values(1, 'Sưa xe', 100000, 'Thay lôp , va lôp, chêt may')
+insert into ServiceTb values(2, 'Hêt xăng', 100000, 'Mua xăng, dăt xe')
+insert into ServiceTb values(3, 'Hêt binh', 100000, 'Sac binh, thay binh')
+
+insert into BillTb values(1, '2020-05-19', 100000, 3, 2)
+insert into BillTb values(2, '2020-03-12', 100000, 5, 5)
+insert into BillTb values(3, '2020-01-01', 100000, 3, 3)
+
+insert into ServiceOfPartnerTb values(1, 100000, 1, 1)
+insert into ServiceOfPartnerTb values(2, 100000, 1, 2)
+insert into ServiceOfPartnerTb values(3, 100000, 1, 3)
+insert into ServiceOfPartnerTb values(4, 100000, 2, 1)
+insert into ServiceOfPartnerTb values(5, 100000, 2, 2)
+insert into ServiceOfPartnerTb values(6, 100000, 3, 3)
+
+
+
+
+
 
